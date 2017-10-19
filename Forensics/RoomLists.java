6702686052ed0149d735f6b8a1780f04e5219982
Patrick Orel <P.Orel@bssdstd.net>
2017-10-19 10:11:15 -0500

    Update, fixed stuff.

--- Forensics/RoomLists.java
+++ Forensics/RoomLists.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Collections;
 public class RoomLists
 {
     public static void main(int schools){
-        ArrayList rooms = new ArrayList<ArrayList>(0);
-        String codes = "";
+        ArrayList<ArrayList<String>> rooms = new ArrayList<ArrayList<String>>(0);
+        ArrayList<String> codes = new ArrayList<String>(0);
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         int total, schoolTotal, roomNum, codeNum;
         total = schoolTotal = roomNum = codeNum = 0;
@@ -16,7 +17,7 @@ public class RoomLists
             total += schoolTotal;
             if (schoolTotal > 0){
                 for (int c = 0; c < schoolTotal; c++){
-                    codes += (i + 1) + alphabet.substring(c, c+1) + "|";
+                    codes.add((i + 1) + alphabet.substring(c, c+1));
                 }
             }
             System.out.println(codes);
@@ -36,11 +37,17 @@ public class RoomLists
             rooms.add(new ArrayList<String>(0));
             for (int c = 0; c < codeNum; c++){
                 localRoomTotal ++;
-                rooms.get(i).add(0,Integer.toString(c+1));                                 
+                rooms.get(i).add(0,"TBP");                                 
             }
             System.out.println(localRoomTotal);
-            rooms.add(new ArrayList(localRoomTotal));
         }
         System.out.println(rooms);
+        // Sorting algarithm for round 1 sorting
+        ArrayList<String> round1 = codes;
+        for (String code : round1){
+            String schoolNum = code.substring(0,1);
+            String value = code.substring(1);
+            ArrayList<ArrayList<String>> roomPreference = Collections.shuffle(rooms);
+        }
     }
 }
